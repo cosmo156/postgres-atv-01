9591b9b921f30994a488f3886355a55fc7e7f55b
Cosmode <>
2021-11-01 18:44:22 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const { Pool } = require('pg');
+
+async function conexao(){
+  const pool = new Pool({
+    connectionString: process.env['db_url'],
+
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+  await pool.connect();
+  const res = await pool.query('SELECT * from cosmo')
+  console.log(res.rows);
+  
+  /*await pool.query(
+    `Create table cosmo(
+      id serial primary key,
+      nome varchar
+     );`);
+*/
+  pool.release();
+
+  
+}
+conexao();
